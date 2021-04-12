2ecd03adc548adb5e2bc83f1db2978e51323faf6
bsoft <zhanglik@bsoft.com.cn>
2021-04-12 15:00:22 +0800

    初始化init

---
+++ ZKGCDTimer/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ZKGCDTimer
+//
+//  Created by bsoft on 2021/4/12.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZKGCDTimer/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  ZKGCDTimer
+//
+//  Created by bsoft on 2021/4/12.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
